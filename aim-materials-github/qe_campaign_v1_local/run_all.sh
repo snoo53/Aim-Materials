5ff33127ac5de8235dbc7bc2ebaf8a72c1d59acb
Sunwoo Lee <sunwoolee53@gmail.com>
2026-02-22 00:01:55 +0900

    Delete aim-materials-github directory

--- aim-materials-github/qe_campaign_v1_local/run_all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-QE_CMD="${1:-pw.x}"
-NPROC="${2:-1}"
-
-bash run_relax.sh "$QE_CMD" "$NPROC"
-bash run_scf.sh "$QE_CMD" "$NPROC"
-bash run_elastic.sh "$QE_CMD" "$NPROC"
